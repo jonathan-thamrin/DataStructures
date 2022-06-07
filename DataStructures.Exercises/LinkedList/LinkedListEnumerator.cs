156810bca5982faeb21f24f80ca862c7e6587cb4
Jonathan Thamrin <jonathan.thamrin@myob.com>
2022-06-08 10:11:54 +1200

    Enumerator

--- DataStructures.Exercises/LinkedList/LinkedListEnumerator.cs
+++ DataStructures.Exercises/LinkedList/LinkedListEnumerator.cs
@@ -1,6 +1,40 @@
+using System.Collections;
+
 namespace DataStructures.Exercises.LinkedList;
 
-public class LinkedListEnumerator
+public class LinkedListEnumerator<T> : IEnumerator<T>
 {
-    
+    private LinkedList<T> _linkedList;
+    private int currentIndex;
+    private T currentItem;
+
+    public LinkedListEnumerator(LinkedList<T> linkedList)
+    {
+        _linkedList = linkedList;
+        currentIndex = -1;
+        currentItem = default;
+    }
+
+    public bool MoveNext()
+    {
+        if (++currentIndex >= _linkedList.Count)
+        {
+            return false;
+        }
+        
+        currentItem = _linkedList[currentIndex];
+        
+        return true;
+    }
+
+    public void Reset()
+    {
+        currentIndex = -1;
+    }
+
+    public T Current => currentItem;
+
+    object IEnumerator.Current => Current;
+
+    void IDisposable.Dispose() { }
 }
